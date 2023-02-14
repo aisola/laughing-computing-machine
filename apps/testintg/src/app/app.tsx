67ece5c39fb7738bd84e49b1b26e20503ca38c53
Abram Isola <abram@isola.dev>
2023-02-14 09:01:26 -0600

    initial commit

--- apps/testintg/src/app/app.tsx
+++ apps/testintg/src/app/app.tsx
@@ -1,11 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
+import { Ui } from "@testing/ui";
 
+import styles from './app.module.css';
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <>
+      <Ui />
+
       <NxWelcome title="testintg" />
 
       <div />
